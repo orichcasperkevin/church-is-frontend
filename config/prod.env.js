71ac39cdd776660a008c952aaedfb68021bfa0ee
mugambi <mugagambi@gmail.com>
2019-06-10 01:30:38 +0300

    base

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   //change this to production URL 
-  BASE_URL: '"https://church.nanocomputing.co.ke/"'
+  BASE_URL: '"https://church.nanocomputing.co.ke"'
 }
